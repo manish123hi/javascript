21edad21c574ee028f398a7f6e8bbf6152275261
manish123hi <65812794+manish123hi@users.noreply.github.com>
2024-05-13 14:45:16 +0000

    this is all about immediately invoked function expression(iief

---
+++ basics/js9.js
@@ -0,0 +1,22 @@
+// immediately invoked function expression(iief)
+(function chia() {
+    console.log(`man`);
+})
+();
+//yesko lagi immediate callko lagi taileyesari dui ota ()(); use garnu parxa teo `;`ramrai usee grana sik hai ta vai 
+// similarly talaui arrow function ko funtion ko pani use garna sakxa hai ta kta 
+
+(
+    (name)=>{
+        console.log(`hello dear ${name}`);
+    }
+)("manish");
+
+// jaba ta $ ko use garxa teo case ma `` use garnu knai paraxa that\n s compulsion
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// how does code execute in js?
+ 
